1629fad8db3787523c0f8224e8fc914ed6f61912
rayhan926 <rayhanbd7075@gmail.com>
2021-11-16 10:49:50 +0600

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,90 +1,90 @@
 import axios from 'axios';
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import Table from './react-table/Table';
 
-const columns = [
-    {
-        Header: 'Order id',
-        accessor: 'id',
-    },
-    {
-        Header: 'User id',
-        accessor: 'user_id',
-    },
-    // {
-    //     Header: 'Ingame id',
-    //     accessor: 'ingameid',
-    // },
-    {
-        Header: 'Player id',
-        accessor: 'playerid',
-    },
-    {
-        Header: 'Password',
-        accessor: 'ingamepassword',
-    },
-    {
-        Header: 'Package name',
-        accessor: 'name',
-    },
-    {
-        Header: 'Price',
-        accessor: 'amount',
-    },
-    {
-        Header: 'Account type',
-        accessor: 'accounttype',
-    },
-    {
-        Header: 'Security code',
-        accessor: 'securitycode',
-    },
-    // {
-    //     Header: 'product_id',
-    //     accessor: 'product_id',
-    // },
-    {
-        Header: 'Phone',
-        accessor: 'phone',
-    },
-    {
-        Header: 'Status',
-        accessor: 'status',
-    },
-    {
-        Header: 'Completed by',
-        accessor: 'completed_by',
-    },
-    {
-        Header: 'Created at',
-        accessor: 'created_at',
-    },
+// const columns = [
+//     {
+//         Header: 'Order id',
+//         accessor: 'id',
+//     },
+//     {
+//         Header: 'User id',
+//         accessor: 'user_id',
+//     },
+//     // {
+//     //     Header: 'Ingame id',
+//     //     accessor: 'ingameid',
+//     // },
+//     {
+//         Header: 'Player id',
+//         accessor: 'playerid',
+//     },
+//     {
+//         Header: 'Password',
+//         accessor: 'ingamepassword',
+//     },
+//     {
+//         Header: 'Package name',
+//         accessor: 'name',
+//     },
+//     {
+//         Header: 'Price',
+//         accessor: 'amount',
+//     },
+//     {
+//         Header: 'Account type',
+//         accessor: 'accounttype',
+//     },
+//     {
+//         Header: 'Security code',
+//         accessor: 'securitycode',
+//     },
+//     // {
+//     //     Header: 'product_id',
+//     //     accessor: 'product_id',
+//     // },
+//     {
+//         Header: 'Phone',
+//         accessor: 'phone',
+//     },
+//     {
+//         Header: 'Status',
+//         accessor: 'status',
+//     },
+//     {
+//         Header: 'Completed by',
+//         accessor: 'completed_by',
+//     },
+//     {
+//         Header: 'Created at',
+//         accessor: 'created_at',
+//     },
 
-    // {
-    //     Header: 'payment_mathod',
-    //     accessor: 'payment_mathod',
-    // },
-    // {
-    //     Header: 'payment_status',
-    //     accessor: 'payment_status',
-    // },
-    // {
-    //     Header: 'payment_data',
-    //     accessor: 'payment_data',
-    // },
-    // {
-    //     Header: 'brief_note',
-    //     accessor: 'brief_note',
-    // },
-    // {
-    //     Header: 'topuppackage_id',
-    //     accessor: 'topuppackage_id',
-    // },
-    // {
-    //     Header: 'bprice',
-    //     accessor: 'bprice',
-    // },
-];
+//     // {
+//     //     Header: 'payment_mathod',
+//     //     accessor: 'payment_mathod',
+//     // },
+//     // {
+//     //     Header: 'payment_status',
+//     //     accessor: 'payment_status',
+//     // },
+//     // {
+//     //     Header: 'payment_data',
+//     //     accessor: 'payment_data',
+//     // },
+//     // {
+//     //     Header: 'brief_note',
+//     //     accessor: 'brief_note',
+//     // },
+//     // {
+//     //     Header: 'topuppackage_id',
+//     //     accessor: 'topuppackage_id',
+//     // },
+//     // {
+//     //     Header: 'bprice',
+//     //     accessor: 'bprice',
+//     // },
+// ];
 
 const todosColumns = [
     {
@@ -106,21 +106,19 @@ const todosColumns = [
     },
 ];
 
-const dataFetcher = async (q) => {
-    try {
-        const res = await axios.get(`http://localhost:3005/api/admin/orders?${q}`);
-        return {
-            data: res.data.data.orders,
-            total: res.data.data.order_count,
-        };
-    } catch (err) {
-        return err?.response?.data?.message;
-    }
-};
+// const dataFetcher = async (q) => {
+//     try {
+//         const res = await axios.get(`http://localhost:3005/api/admin/orders?${q}`);
+//         return {
+//             data: res.data.data.orders,
+//             total: res.data.data.order_count,
+//         };
+//     } catch (err) {
+//         return err?.response?.data?.message;
+//     }
+// };
 
 function App() {
-    const [todos, setSodos] = useState([]);
-
     const fetchTodos = useCallback(async (q, d) => {
         try {
             const res = await axios.get(
@@ -129,7 +127,6 @@ function App() {
                 }&_limit=${d.limit}`
             );
             const result = res.data;
-            setSodos(result);
 
             return {
                 data: result,
